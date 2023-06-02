55ca3c4aa11080c6decfce058b26d5a1c39d0870
lejlamrt <lejlamuratovic6@gmail.com>
2023-06-03 00:28:02 +0200

    h

---
+++ navigation.js
@@ -0,0 +1,5 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+hamburgerMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("show");
+});
